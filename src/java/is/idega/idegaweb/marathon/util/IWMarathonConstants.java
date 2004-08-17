395fe2c659b44c0d08fc843fc1476a09ef8e5516
Thorhallur Helgason <laddi@idega.com>
2004-08-17 11:23:56 +0000

    Changed static ints to final.

--- src/java/is/idega/idegaweb/marathon/util/IWMarathonConstants.java
+++ src/java/is/idega/idegaweb/marathon/util/IWMarathonConstants.java
@@ -125,18 +125,18 @@ public class IWMarathonConstants {
   public static final String MALE_50_59 = "male_50_59";
   public static final String MALE_60 = "male_60";
   
-  public static int MAX_NUMBER_DISTANCE_55 = 999;
-  public static int MIN_NUMBER_DISTANCE_55 = 1;
-  public static int MAX_NUMBER_DISTANCE_42 = 999;
-  public static int MIN_NUMBER_DISTANCE_42 = 1;
-  public static int MAX_NUMBER_DISTANCE_21 = 1999;
-  public static int MIN_NUMBER_DISTANCE_21 = 1000;
-  public static int MAX_NUMBER_DISTANCE_10 = 3999;
-  public static int MIN_NUMBER_DISTANCE_10 = 2000;
-  public static int MAX_NUMBER_DISTANCE_7 = 4999;
-  public static int MIN_NUMBER_DISTANCE_7 = 4000;
-  public static int MAX_NUMBER_DISTANCE_5 = 1999;
-  public static int MIN_NUMBER_DISTANCE_5 = 1;
-  public static int MAX_NUMBER_DISTANCE_3 = 9999;
-  public static int MIN_NUMBER_DISTANCE_3 = 5000;
+  public static final int MAX_NUMBER_DISTANCE_55 = 999;
+  public static final int MIN_NUMBER_DISTANCE_55 = 1;
+  public static final int MAX_NUMBER_DISTANCE_42 = 999;
+  public static final int MIN_NUMBER_DISTANCE_42 = 1;
+  public static final int MAX_NUMBER_DISTANCE_21 = 1999;
+  public static final int MIN_NUMBER_DISTANCE_21 = 1000;
+  public static final int MAX_NUMBER_DISTANCE_10 = 3999;
+  public static final int MIN_NUMBER_DISTANCE_10 = 2000;
+  public static final int MAX_NUMBER_DISTANCE_7 = 4999;
+  public static final int MIN_NUMBER_DISTANCE_7 = 4000;
+  public static final int MAX_NUMBER_DISTANCE_5 = 1999;
+  public static final int MIN_NUMBER_DISTANCE_5 = 1;
+  public static final int MAX_NUMBER_DISTANCE_3 = 9999;
+  public static final int MIN_NUMBER_DISTANCE_3 = 5000;
 }
