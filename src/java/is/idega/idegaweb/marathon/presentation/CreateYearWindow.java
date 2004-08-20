2f39127da6cc42c058866bc2f5931985b658d14a
Jonas Kari Blandon <jonas@idega.com>
2004-08-20 15:53:07 +0000

    added bundle id

--- src/java/is/idega/idegaweb/marathon/presentation/CreateYearWindow.java
+++ src/java/is/idega/idegaweb/marathon/presentation/CreateYearWindow.java
@@ -4,6 +4,7 @@
 package is.idega.idegaweb.marathon.presentation;
 
 import is.idega.idegaweb.marathon.business.RunBusiness;
+import is.idega.idegaweb.marathon.util.IWMarathonConstants;
 
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
@@ -21,54 +22,60 @@ import com.idega.user.data.Group;
  * Description: <br>
  * Copyright: Idega Software 2004 <br>
  * Company: Idega Software <br>
+ * 
  * @author birna
  */
-public class CreateYearWindow extends StyledIWAdminWindow{
-  
-  public void main(IWContext iwc) throws Exception{
-    IWResourceBundle iwrb = getResourceBundle(iwc);
-    String runID = iwc.getParameter("ic_group_id");
-    
-    Group run = null;
-    if(runID!=null && !runID.equals("")) {
-      int id = Integer.parseInt(runID);
-      run = getGroupBiz(iwc).getGroupByGroupID(id);
-    }
-    
-    
-    SubmitButton create = new SubmitButton(iwrb.getLocalizedString("run_reg.submit_step_one","Next step"),"action",String.valueOf(1));
-    Table table = new Table();
-    Form form = new Form();
-    form.maintainParameter("ic_group_id");
-    table.add(new TextInput("year"),1,1);
-    table.add(create,1,2);
-    form.add(table);
-    add(form,iwc);
-    switch(parseAction(iwc)) {
-    case 1:
-      getRunBiz(iwc).createNewGroupYear(iwc,run,iwc.getParameter("year"));
-      close();
-    }
-  }
-  private int parseAction(IWContext iwc) {
-    if (iwc.isParameterSet("action")) {
-      return Integer.parseInt(iwc.getParameter("action"));
-    }
-    else {
-      return 0;
-    }
-  }
-  private RunBusiness getRunBiz(IWContext iwc){
-    RunBusiness business = null;
-    try{
-      business = (RunBusiness) IBOLookup.getServiceInstance(iwc,RunBusiness.class);
-    }catch(IBOLookupException e) {
-      business = null;
-    }
-    return business;  
-  }
-  private GroupBusiness getGroupBiz(IWContext iwc) throws IBOLookupException {
-    GroupBusiness business = (GroupBusiness) IBOLookup.getServiceInstance(iwc,GroupBusiness.class);
-    return business;
-  }
+public class CreateYearWindow extends StyledIWAdminWindow {
+
+	public void main(IWContext iwc) throws Exception {
+		IWResourceBundle iwrb = getResourceBundle(iwc);
+		String runID = iwc.getParameter("ic_group_id");
+
+		Group run = null;
+		if (runID != null && !runID.equals("")) {
+			int id = Integer.parseInt(runID);
+			run = getGroupBiz(iwc).getGroupByGroupID(id);
+		}
+
+		SubmitButton create = new SubmitButton(iwrb.getLocalizedString("run_reg.submit_step_one", "Next step"), "action", String.valueOf(1));
+		Table table = new Table();
+		Form form = new Form();
+		form.maintainParameter("ic_group_id");
+		table.add(new TextInput("year"), 1, 1);
+		table.add(create, 1, 2);
+		form.add(table);
+		add(form, iwc);
+		switch (parseAction(iwc)) {
+			case 1:
+				getRunBiz(iwc).createNewGroupYear(iwc, run, iwc.getParameter("year"));
+				close();
+		}
+	}
+
+	private int parseAction(IWContext iwc) {
+		if (iwc.isParameterSet("action")) {
+			return Integer.parseInt(iwc.getParameter("action"));
+		} else {
+			return 0;
+		}
+	}
+
+	private RunBusiness getRunBiz(IWContext iwc) {
+		RunBusiness business = null;
+		try {
+			business = (RunBusiness) IBOLookup.getServiceInstance(iwc, RunBusiness.class);
+		} catch (IBOLookupException e) {
+			business = null;
+		}
+		return business;
+	}
+
+	private GroupBusiness getGroupBiz(IWContext iwc) throws IBOLookupException {
+		GroupBusiness business = (GroupBusiness) IBOLookup.getServiceInstance(iwc, GroupBusiness.class);
+		return business;
+	}
+
+	public String getBundleIdentifier() {
+		return IWMarathonConstants.IW_BUNDLE_IDENTIFIER;
+	}
 }
