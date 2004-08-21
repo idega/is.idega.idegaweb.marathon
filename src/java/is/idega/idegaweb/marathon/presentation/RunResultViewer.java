813cb1b678655f6ff8fc18ec316ebb5da3c9f682
Thorhallur Helgason <laddi@idega.com>
2004-08-21 12:15:34 +0000

    Fixed table insertion.

--- src/java/is/idega/idegaweb/marathon/presentation/RunResultViewer.java
+++ src/java/is/idega/idegaweb/marathon/presentation/RunResultViewer.java
@@ -36,7 +36,9 @@ import com.idega.user.data.User;
 import com.idega.util.IWColor;
 
 /**
- * Show the results of a run. This block can show results for a specific run, year and run group
+ * Show the results of a run. This block can show results for a specific run,
+ * year and run group
+ * 
  * @author birna
  */
 public class RunResultViewer extends Block {
@@ -44,27 +46,39 @@ public class RunResultViewer extends Block {
 	private static final String STYLENAME_INTERFACE = "interface";
 
 	private static final int COLUMN_COUNT = 7;
+
 	private static final String HEADLINE_BACKGROUND_COLOR = "454545";
+
 	private static final String HEADLINE_COLOR = "FFFFFF";
+
 	private static final int HEADLINE_SIZE = 12;
 
 	private static String _groupYear;
+
 	private static String _groupDistance;
-	
+
 	private Group distance;
+
 	private Group year;
-	
+
 	private Map _runToRunnerMap = null;
+
 	private Collator _collator;
+
 	private IWContext _iwc;
+
 	private IWResourceBundle iwrb;
+
 	private RunBusiness _runBiz;
+
 	private GroupBusiness _groupBiz;
-	
+
 	private String runPK;
+
 	private Group run;
+
 	private SelectorUtility util;
-	
+
 	public void main(IWContext iwc) throws Exception {
 		_iwc = iwc;
 		iwrb = getResourceBundle(iwc);
@@ -74,36 +88,36 @@ public class RunResultViewer extends Block {
 		if (runPK != null) {
 			run = getGroupBiz().getGroupByGroupID(Integer.parseInt(runPK));
 		}
-		
+
 		if (run == null) {
 			add("No run set...");
 			return;
 		}
-		
-		_groupYear = iwc.getParameter(IWMarathonConstants.GROUP_TYPE_RUN_YEAR);
-		if (_groupYear != null) {
+
+		if (iwc.isParameterSet(IWMarathonConstants.GROUP_TYPE_RUN_YEAR)) {
 			try {
-				year = getGroupBiz().getGroupByGroupID(Integer.parseInt(_groupYear));
-			} catch (RemoteException e) {
+				year = getGroupBiz().getGroupByGroupID(Integer.parseInt(iwc.getParameter(IWMarathonConstants.GROUP_TYPE_RUN_YEAR)));
+			}
+			catch (RemoteException e) {
 				e.printStackTrace();
-			} catch (FinderException e) {
+			}
+			catch (FinderException e) {
 				e.printStackTrace();
 			}
 		}
-		
-		_groupDistance = iwc.getParameter(IWMarathonConstants.GROUP_TYPE_RUN_DISTANCE);
-		int groupID = -1;
-		if (_groupDistance != null && !_groupDistance.equals("")) {
-			groupID = Integer.parseInt(_groupDistance);
+
+		if (iwc.isParameterSet(IWMarathonConstants.GROUP_TYPE_RUN_DISTANCE)) {
+			try {
+				distance = getGroupBiz().getGroupByGroupID(Integer.parseInt(iwc.getParameter(IWMarathonConstants.GROUP_TYPE_RUN_DISTANCE)));
+			}
+			catch (RemoteException e) {
+				e.printStackTrace();
+			}
+			catch (FinderException e) {
+				e.printStackTrace();
+			}
 		}
-		try {
-			distance = getGroupBiz().getGroupByGroupID(groupID);
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		} catch (FinderException e) {
-			e.printStackTrace();
-		}
-		
+
 		Form form = new Form();
 		Table table = new Table();
 
@@ -111,7 +125,7 @@ public class RunResultViewer extends Block {
 		form.add(getDistanceDropdown());
 		form.add(getSortDropdown());
 		form.add(new Break(2));
-		
+
 		if (distance != null) {
 			try {
 				List runGroups = new ArrayList(getGroupBiz().getChildGroups(distance));
@@ -121,7 +135,7 @@ public class RunResultViewer extends Block {
 				while (runGroupIter.hasNext()) {
 					Group runGroup = (Group) runGroupIter.next();
 					row = insertRunGroupIntoTable(table, row, runGroup);
-	
+
 					List runners = new ArrayList(getGroupBiz().getUsers(runGroup));
 					List runs = getRunsForRunners(runners);
 					sortRuns(runs);
@@ -141,7 +155,7 @@ public class RunResultViewer extends Block {
 		form.add(table);
 		add(form);
 	}
-	
+
 	private DropdownMenu getYearsDropdown() throws RemoteException {
 		DropdownMenu years = (DropdownMenu) util.getSelectorFromIDOEntities(new DropdownMenu(IWMarathonConstants.GROUP_TYPE_RUN_YEAR), getRunBiz().getYears(run), "getName", iwrb);
 		years.addMenuElementFirst("", "");
@@ -149,16 +163,16 @@ public class RunResultViewer extends Block {
 		years.keepStatusOnAction();
 		return years;
 	}
-	
+
 	private DropdownMenu getDistanceDropdown() throws RemoteException {
 		DropdownMenu distanceMenu = new DropdownMenu(IWMarathonConstants.GROUP_TYPE_RUN_DISTANCE);
 		distanceMenu.setToSubmit();
 		distanceMenu.keepStatusOnAction();
 		distanceMenu.addMenuElementFirst("", "");
-		
+
 		Integer threeKM = new Integer(iwrb.getIWBundleParent().getProperty("3_km_id", "126"));
 		Integer sevenKM = new Integer(iwrb.getIWBundleParent().getProperty("7_km_id", "113"));
-		
+
 		if (year != null) {
 			List distances = getRunBiz().getDistancesMap(run, year.getName());
 			Iterator iter = distances.iterator();
@@ -169,49 +183,55 @@ public class RunResultViewer extends Block {
 				}
 			}
 		}
-		
+
 		return distanceMenu;
 	}
-	
+
 	private DropdownMenu getSortDropdown() throws RemoteException {
-    DropdownMenu sort = new DropdownMenu(IWMarathonConstants.PARAMETER_SORT_BY);
-    sort.addMenuElement(IWMarathonConstants.PARAMETER_TOTAL,iwrb.getLocalizedString(IWMarathonConstants.RYSDD_TOTAL,"Total result list"));
-    sort.addMenuElement(IWMarathonConstants.PARAMETER_GROUPS,iwrb.getLocalizedString(IWMarathonConstants.RYSDD_GROUPS,"Groups"));
-    sort.addMenuElement(IWMarathonConstants.PARAMETER_GROUPS_COMPETITION,iwrb.getLocalizedString(IWMarathonConstants.RYSDD_GROUPS_COMP,"Group competition"));
-    sort.setToSubmit();
-    sort.keepStatusOnAction();
-    
-    return sort;
+		DropdownMenu sort = new DropdownMenu(IWMarathonConstants.PARAMETER_SORT_BY);
+		sort.addMenuElement(IWMarathonConstants.PARAMETER_TOTAL, iwrb.getLocalizedString(IWMarathonConstants.RYSDD_TOTAL, "Total result list"));
+		sort.addMenuElement(IWMarathonConstants.PARAMETER_GROUPS, iwrb.getLocalizedString(IWMarathonConstants.RYSDD_GROUPS, "Groups"));
+		sort.addMenuElement(IWMarathonConstants.PARAMETER_GROUPS_COMPETITION, iwrb.getLocalizedString(IWMarathonConstants.RYSDD_GROUPS_COMP, "Group competition"));
+		sort.setToSubmit();
+		sort.keepStatusOnAction();
+
+		return sort;
 	}
-	
+
 	/**
-	 * Gets the Run objects for a collection of runners, also creates the <code>_runToRunnerMap</code> map.
+	 * Gets the Run objects for a collection of runners, also creates the
+	 * <code>_runToRunnerMap</code> map.
 	 * 
-	 * @param runners A Collection of runners
+	 * @param runners
+	 *            A Collection of runners
 	 * @return A Collection of Runs
 	 */
 	private List getRunsForRunners(List runners) {
 		List runs = new ArrayList(runners.size());
 		_runToRunnerMap = new HashMap();
-		
+
 		Iterator runnerIter = runners.iterator();
 		while (runnerIter.hasNext()) {
 			User runner = (User) runnerIter.next();
-			Run run = getRunBiz().getRunObjByUserIDandDistanceID(((Integer) runner.getPrimaryKey()).intValue(), ((Integer) distance.getPrimaryKey()).intValue()); // @TODO (jonas) get run using biz method birna will create
-			_runToRunnerMap.put(run, runner);
-			runs.add(run);
+			Run run = getRunBiz().getRunObjByUserIDandDistanceID(((Integer) runner.getPrimaryKey()).intValue(), ((Integer) distance.getPrimaryKey()).intValue()); // @TODO
+			if (run != null) {
+				_runToRunnerMap.put(run, runner);
+				runs.add(run);
+			}
 		}
 
 		return runs;
 	}
-	
+
 	/**
 	 * Sorts runs by their time
 	 * 
-	 * @param runs The list of Run objects to sort
+	 * @param runs
+	 *            The list of Run objects to sort
 	 */
 	private void sortRuns(List runs) {
 		Collections.sort(runs, new Comparator() {
+
 			public int compare(Object arg0, Object arg1) {
 				Run r0 = (Run) arg0;
 				Run r1 = (Run) arg1;
@@ -222,10 +242,13 @@ public class RunResultViewer extends Block {
 
 	/**
 	 * Sorts groups based on their names
-	 * @param groups The list of groups to sort
+	 * 
+	 * @param groups
+	 *            The list of groups to sort
 	 */
 	private void sortRunnerGroups(List groups) {
 		Collections.sort(groups, new Comparator() {
+
 			public int compare(Object arg0, Object arg1) {
 				Group g0 = (Group) arg0;
 				Group g1 = (Group) arg1;
@@ -242,9 +265,9 @@ public class RunResultViewer extends Block {
 		String chipTime = getTimeStringFromMillis(run.getChipTime());
 		table.add(getRunnerRowText(chipTime), 3, row);
 		table.add(getRunnerRowText(user.getName()), 4, row);
-		table.add(getRunnerRowText(Integer.toString(user.getDateOfBirth().getYear())));
-		table.add(getRunnerRowText(run.getUserNationality()));
-		table.add(getRunnerRowText(run.getRunGroupName()));
+		table.add(getRunnerRowText(Integer.toString(user.getDateOfBirth().getYear())), 5, row);
+		table.add(getRunnerRowText(run.getUserNationality()), 6, row);
+		table.add(getRunnerRowText(run.getRunGroupName()), 7, row);
 		return row++;
 	}
 
@@ -281,10 +304,11 @@ public class RunResultViewer extends Block {
 	}
 
 	private RunBusiness getRunBiz() {
-		if(_runBiz == null) {
+		if (_runBiz == null) {
 			try {
 				_runBiz = (RunBusiness) IBOLookup.getServiceInstance(_iwc, RunBusiness.class);
-			} catch (IBOLookupException e) {
+			}
+			catch (IBOLookupException e) {
 				e.printStackTrace();
 			}
 		}
@@ -292,10 +316,11 @@ public class RunResultViewer extends Block {
 	}
 
 	private GroupBusiness getGroupBiz() {
-		if(_groupBiz == null) {
+		if (_groupBiz == null) {
 			try {
 				_groupBiz = (GroupBusiness) IBOLookup.getServiceInstance(_iwc, GroupBusiness.class);
-			} catch (IBOLookupException e) {
+			}
+			catch (IBOLookupException e) {
 				e.printStackTrace();
 			}
 		}
@@ -303,7 +328,8 @@ public class RunResultViewer extends Block {
 	}
 
 	/**
-	 * @param run The run to set.
+	 * @param run
+	 *            The run to set.
 	 */
 	public void setRun(String runPK) {
 		this.runPK = runPK;
