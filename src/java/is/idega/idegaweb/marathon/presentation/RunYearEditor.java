341927f75fbddab2a45a6715877d13d70e8978da
Sigtryggur Simonarson <sigtryggur@idega.com>
2007-05-03 22:18:03 +0000

    Various layout and logic changes

--- src/java/is/idega/idegaweb/marathon/presentation/RunYearEditor.java
+++ src/java/is/idega/idegaweb/marathon/presentation/RunYearEditor.java
@@ -1,11 +1,15 @@
 package is.idega.idegaweb.marathon.presentation;
 
+import is.idega.idegaweb.marathon.business.RunBusiness;
 import is.idega.idegaweb.marathon.util.IWMarathonConstants;
 
 import java.rmi.RemoteException;
 import java.util.Collection;
 import java.util.Iterator;
 
+
+import com.idega.business.IBOLookup;
+import com.idega.business.IBOLookupException;
 import com.idega.presentation.IWContext;
 import com.idega.presentation.Layer;
 import com.idega.presentation.Table2;
@@ -23,9 +27,9 @@ import com.idega.presentation.ui.SubmitButton;
 import com.idega.presentation.ui.TextInput;
 import com.idega.presentation.ui.TimestampInput;
 import com.idega.user.data.Group;
+import com.idega.util.IWTimestamp;
 
 public class RunYearEditor extends RunBlock {
-	
 	
 	private static final String PARAMETER_ACTION = "marathon_prm_action";
 	private static final String PARAMETER_MARATHON_PK = "prm_run_pk";
@@ -34,11 +38,15 @@ public class RunYearEditor extends RunBlock {
 	private static final String PARAMETER_RUN_DATE = "prm_run_date";
 	private static final String PARAMETER_LAST_REGISTRATION_DATE = "prm_last_registration_date";
 	
+	private static final String METADATA_RUN_DATE = "run_date";
+	private static final String METADATA_LAST_REGISTRATION_DATE = "last_registration_date";
+	
 	private static final int ACTION_VIEW = 1;
 	private static final int ACTION_EDIT = 2;
 	private static final int ACTION_NEW = 3;
-	private static final int ACTION_SAVE = 4;
-	private static final int ACTION_DELETE = 5;
+	private static final int ACTION_SAVE_NEW = 4;
+	private static final int ACTION_SAVE_EDIT = 5;
+	private static final int ACTION_DELETE = 6;
 	
 
 	public void main(IWContext iwc) throws Exception {
@@ -51,22 +59,26 @@ public class RunYearEditor extends RunBlock {
 				showList(iwc);
 				break;
 
-			case ACTION_EDIT:
-				Object runYearPK = iwc.getParameter(PARAMETER_MARATHON_YEAR_PK);
-				showEditor(iwc, runYearPK);
-				break;
-
 			case ACTION_NEW:
-				showEditor(iwc, null);
+				showNewEditor(iwc);
 				break;
 
-			case ACTION_SAVE:
-				saveYear(iwc);
+			case ACTION_EDIT:
+				showEditor(iwc);
+				break;
+
+			case ACTION_SAVE_NEW:
+				saveNew(iwc);
+				showList(iwc);
+				break;
+
+			case ACTION_SAVE_EDIT:
+				saveEdit(iwc);
 				showList(iwc);
 				break;
 
 			case ACTION_DELETE:
-				//TODO implement delete functions if needed
+				//COULD-DO: implement delete functions if needed
 				showList(iwc);
 				break;
 		}
@@ -124,43 +136,45 @@ public class RunYearEditor extends RunBlock {
 		int iRow = 1;
 		
 		if (years != null) {
-		Iterator iter = years.iterator();
-		Group run;
-		
-		while (iter.hasNext()) {
-			row = group.createRow();
-			run = (Group) iter.next();
-			try {
-				Link edit = new Link(getEditIcon(localize("edit", "Edit")));
-				edit.addParameter(PARAMETER_MARATHON_YEAR_PK, run.getPrimaryKey().toString());
-				edit.addParameter(PARAMETER_ACTION, ACTION_EDIT);
-							
-				cell = row.createCell();
-				cell.add(new Text(run.getName()));
-				row.createCell().add(edit);
+			Iterator iter = years.iterator();
+			Group run;
+			
+			while (iter.hasNext()) {
+				row = group.createRow();
+				run = (Group) iter.next();
+				try {
+					Link edit = new Link(getEditIcon(localize("edit", "Edit")));
+					edit.addParameter(PARAMETER_MARATHON_YEAR_PK, run.getPrimaryKey().toString());
+					edit.addParameter(PARAMETER_ACTION, ACTION_EDIT);
+								
+					cell = row.createCell();
+					cell.add(new Text(run.getName()));
+					row.createCell().add(edit);
+				}
+				catch (Exception ex) {
+					ex.printStackTrace();
+				}
+				iRow++;
 			}
-			catch (Exception ex) {
-				ex.printStackTrace();
-			}
-			iRow++;
-		}
-		}
-		if (iwc.isParameterSet(PARAMETER_MARATHON_PK)) {
-			SubmitButton newLink = (SubmitButton) getButton(new SubmitButton(localize("new_year", "New year"), PARAMETER_ACTION, String.valueOf(ACTION_NEW)));
-			group = table.createFooterRowGroup();
-			group.createRow().createCell().setHeight("20");
-			row = group.createRow();
-			cell = row.createCell();
-			cell.add(newLink);
 		}
 		form.add(table);
 		form.add(new Break());
+		if (iwc.isParameterSet(PARAMETER_MARATHON_PK)) {
+			SubmitButton newLink = (SubmitButton) getButton(new SubmitButton(localize("new_year", "New year"), PARAMETER_ACTION, String.valueOf(ACTION_NEW)));
+			form.add(newLink);
+		}
 		add(form);
 	}
 
-	public void showEditor(IWContext iwc, Object runYearPK) throws java.rmi.RemoteException {
-		Form form = new Form();
+	public void showNewEditor(IWContext iwc) throws java.rmi.RemoteException {
+		String runID = iwc.getParameter(PARAMETER_MARATHON_PK);
+		CreateYearForm form = new CreateYearForm(runID);
+		add(form);
+	}
 
+	public void showEditor(IWContext iwc) throws java.rmi.RemoteException {
+		String yearID = iwc.getParameter(PARAMETER_MARATHON_YEAR_PK);
+		Form form = new Form();
 		TextInput year = new TextInput(PARAMETER_YEAR);
 		DateInput runDate = new DateInput(PARAMETER_RUN_DATE);
 		TimestampInput lastRegistrationDate = new TimestampInput(PARAMETER_LAST_REGISTRATION_DATE);
@@ -190,16 +204,44 @@ public class RunYearEditor extends RunBlock {
 		form.add(layer);
 		form.add(new Break());
 
-		SubmitButton save = (SubmitButton) getButton(new SubmitButton(localize("save", "Save"), PARAMETER_ACTION, String.valueOf(ACTION_SAVE)));
+		SubmitButton save = (SubmitButton) getButton(new SubmitButton(localize("save", "Save"), PARAMETER_ACTION, String.valueOf(ACTION_SAVE_EDIT)));
 		SubmitButton cancel = (SubmitButton) getButton(new SubmitButton(localize("cancel", "Cancel"), PARAMETER_ACTION, String.valueOf(ACTION_VIEW)));
 
 		form.add(save);
 		form.add(cancel);
-
+		
+		if (yearID != null) {
+			Group selectedYear = getRunBusiness(iwc).getRunGroupByGroupId(Integer.valueOf(yearID.toString()));
+			year.setValue(selectedYear.getName());
+			year.setDisabled(true);
+			String runDateString = selectedYear.getMetaData(METADATA_RUN_DATE);
+			if (runDateString != null) { 
+				runDate.setDate(new IWTimestamp(runDateString).getDate());
+			}
+			String lastRegistrationString = selectedYear.getMetaData(METADATA_LAST_REGISTRATION_DATE);
+			if (lastRegistrationString != null) { 
+				lastRegistrationDate.setTimestamp(new IWTimestamp(lastRegistrationString).getTimestamp());
+			}
+		}
 		add(form);
 	}
 	
-	private void saveYear(IWContext iwc) throws java.rmi.RemoteException {
+	public void saveNew(IWContext iwc) throws java.rmi.RemoteException {
+		String runID = iwc.getParameter(PARAMETER_MARATHON_PK);
+		getRunBiz(iwc).createNewGroupYear(iwc, runID);
+	}
+	
+	private void saveEdit(IWContext iwc) throws java.rmi.RemoteException {
 		System.out.println("saveYear");
+	}
+
+	private RunBusiness getRunBiz(IWContext iwc) {
+		RunBusiness business = null;
+		try {
+			business = (RunBusiness) IBOLookup.getServiceInstance(iwc, RunBusiness.class);
+		} catch (IBOLookupException e) {
+			business = null;
+		}
+		return business;
 	}
 }
