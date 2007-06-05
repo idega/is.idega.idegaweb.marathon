1864761fdeaba17f6337ddea14814082cff14dc9
Sigtryggur Simonarson <sigtryggur@idega.com>
2007-06-05 01:39:52 +0000

    Removed unnecassary stuff

--- src/java/is/idega/idegaweb/marathon/business/PledgeHolder.java
+++ src/java/is/idega/idegaweb/marathon/business/PledgeHolder.java
@@ -10,21 +10,13 @@ import is.idega.idegaweb.marathon.data.Participant;
 
 public class PledgeHolder {
 	private Participant participant;
-
 	private String firstNameFilter;
 	private String middleNameFilter;
 	private String lastNameFilter;
 	private String personalIDFilter;
 	private String charityFilter;
-	
 	private float pledgeAmount;
-	
 	private String cardHolderName;
-	private String cardholderEmail;
-	private String cardholderPersonalID;
-	
-	private boolean agreeToTerms;
-	
 
 	public String getPersonalIDFilter() {
 		return personalIDFilter;
@@ -82,30 +74,6 @@ public class PledgeHolder {
 		this.cardHolderName = cardHolderName;
 	}
 
-	public String getCardholderPersonalID() {
-		return this.cardholderPersonalID;
-	}
-
-	public void setCardholderPersonalID(String cardholderPersonalID) {
-		this.cardholderPersonalID = cardholderPersonalID;
-	}
-
-	public String getCardholderEmail() {
-		return this.cardholderEmail;
-	}
-
-	public void setCardholderEmail(String cardholderEmail) {
-		this.cardholderEmail = cardholderEmail;
-	}
-
-	public boolean isAgreedToTerms() {
-		return this.agreeToTerms;
-	}
-
-	public void setAgreeToTerms(boolean agreeToTerms) {
-		this.agreeToTerms = agreeToTerms;
-	}
-	
 	public float getPledgeAmount() {
 		return this.pledgeAmount;
 	}
