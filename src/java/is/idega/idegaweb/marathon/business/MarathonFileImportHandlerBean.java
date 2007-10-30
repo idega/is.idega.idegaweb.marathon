2f0948433d9f0c7990b7f45586edbee71ad575f2
idega <programmers@idega.com>
2007-10-30 22:17:58 +0000

    Improved import logic

--- src/java/is/idega/idegaweb/marathon/business/MarathonFileImportHandlerBean.java
+++ src/java/is/idega/idegaweb/marathon/business/MarathonFileImportHandlerBean.java
@@ -8,9 +8,9 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
-import java.util.Vector;
-import javax.ejb.CreateException;
 import javax.ejb.FinderException;
+
+import com.idega.block.importer.business.ImportFileHandler;
 import com.idega.block.importer.data.ImportFile;
 import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
@@ -34,7 +34,7 @@ import com.idega.util.text.TextSoap;
 /**
  * @author gimmi
  */
-public class MarathonFileImportHandlerBean extends IBOServiceBean  implements MarathonFileImportHandler{
+public class MarathonFileImportHandlerBean extends IBOServiceBean  implements MarathonFileImportHandler, ImportFileHandler {
 
 	/**
 	 * Comment for <code>serialVersionUID</code>
@@ -49,6 +49,7 @@ public class MarathonFileImportHandlerBean extends IBOServiceBean  implements Ma
 	Locale englishLocale;
 	Locale icelandicLocale;
 	Collection runs;
+	List failedRecords;
 	
 	public boolean handleRecords() throws RemoteException {
 		this.business = getBusiness(getIWApplicationContext());
@@ -59,8 +60,8 @@ public class MarathonFileImportHandlerBean extends IBOServiceBean  implements Ma
 		this.runs = this.business.getRuns();
 		this.englishLocale = LocaleUtil.getLocale(LocaleSwitcher.englishParameterString);
 		this.icelandicLocale = LocaleUtil.getLocale(LocaleSwitcher.icelandicParameterString);
+		this.failedRecords = new ArrayList();
 		
-		Vector errors = new Vector();
 		if (this.file != null) {
 			String line = (String) this.file.getNextRecord();
 			int counter = 0;
@@ -70,7 +71,7 @@ public class MarathonFileImportHandlerBean extends IBOServiceBean  implements Ma
 					System.out.println("MarathonImportCounter = "+counter);
 				}
 				if (!handleLine(line)) {
-					errors.add(line);
+					failedRecords.add(line);
 				}
 				line = (String) this.file.getNextRecord();
 			}
@@ -78,9 +79,9 @@ public class MarathonFileImportHandlerBean extends IBOServiceBean  implements Ma
 			
 		}
 		
-		if (!errors.isEmpty()) {
+		if (!failedRecords.isEmpty()) {
 			System.out.println("Errors in the following lines :");
-			Iterator iter = errors.iterator();
+			Iterator iter = failedRecords.iterator();
 			while (iter.hasNext()) {
 				System.out.println((String) iter.next());
 			}
@@ -91,7 +92,6 @@ public class MarathonFileImportHandlerBean extends IBOServiceBean  implements Ma
 	private boolean handleLine(String line) {
 		ArrayList values = this.file.getValuesFromRecordString(line);
 		int size = values.size();
-		boolean validLine = true;
 		
 		String participantNumber = (String) values.get(0);
 		String userID = (String) values.get(1);
@@ -113,17 +113,8 @@ public class MarathonFileImportHandlerBean extends IBOServiceBean  implements Ma
 		//String runGroupTime = size >= 18 ? (String) values.get(17) : null;
 		String split1 = size >= 19 ? (String) values.get(18) : null;
 		String split2 = size >= 20 ? (String) values.get(19) : null;
-		
-		int number = -1;
 		try {
-			number = Integer.parseInt(participantNumber);
-		} catch (NumberFormatException n) {
-			System.out.println("not using line... starts with : " +participantNumber);
-			validLine = false;
-		}
-		
-		if (validLine) {
-			
+			int number = Integer.parseInt(participantNumber);
 			Iterator iter = this.countries.iterator();
 			Country country = null;
 			boolean found = false;
@@ -133,151 +124,131 @@ public class MarathonFileImportHandlerBean extends IBOServiceBean  implements Ma
 					break;
 				}
 			}
+		
+			Group runGroup = null;
+			Iterator iterator = this.runs.iterator();
+			while (iterator.hasNext()) {
+				Group element = (Group) iterator.next();
+				if (element.getName().equals(run)) {
+					runGroup = element;
+					break;
+				}
+			}
 			
-			IWTimestamp birth = new IWTimestamp(dateOfBirth);
-
-			try {
-				Group runGroup = null;
-				Iterator iterator = this.runs.iterator();
+			if (runGroup == null) {
+				throw new RuntimeException("Run is not found. Skipping participant with participant nr: " + participantNumber);
+			} else {
+			
+				Group yearGroup = null;
+				iterator = runGroup.getChildrenIterator();
 				while (iterator.hasNext()) {
 					Group element = (Group) iterator.next();
-					if (element.getName().equals(run)) {
-						runGroup = element;
+					if (element.getName().equals(year)) {
+						yearGroup = element;
 						break;
 					}
 				}
 				
-				if (runGroup == null) {
-					System.out.println("Run is not found. Skipping participant with participant nr: " + participantNumber);
-					validLine = false;
-				} else {
-				
-					Group yearGroup = null;
-					iterator = runGroup.getChildrenIterator();
-					while (iterator.hasNext()) {
-						Group element = (Group) iterator.next();
-						if (element.getName().equals(year)) {
-							yearGroup = element;
-							break;
-						}
+				Group distanceGroup = null;
+				Collection distances = this.business.getDistancesMap(runGroup, year);
+				iterator = distances.iterator();
+				while (iterator.hasNext()) {
+					Group element = (Group) iterator.next();
+					if (element.getName().equals(distance)) {
+						distanceGroup = element;
+						break;
 					}
-					
-					Group distanceGroup = null;
-					Collection distances = this.business.getDistancesMap(runGroup, year);
-					iterator = distances.iterator();
-					while (iterator.hasNext()) {
-						Group element = (Group) iterator.next();
-						if (element.getName().equals(distance)) {
-							distanceGroup = element;
-							break;
-						}
-					}
-				
-					User user = null;
-					Participant participant = null;
-					if (!userID.trim().equals("")) {
-						try {
-							user = this.userBusiness.getUser(Integer.valueOf(userID));
-						} catch (Exception e) {
-							System.out.println("User not found by pk, trying to find by other criteria");
-						}
-					}
-					if (user == null) {
-						try {
-							if (personalID.length() > 0) {
-								personalID = TextSoap.findAndReplace(personalID, "-", "");
-								user = this.business.getUserBiz().getUser(personalID);
-							}
-						}
-						catch (FinderException fe) {
-							System.out.println("User not found by personal ID, trying name and date of birth...");
-						}
-					}
-					if (user == null) {
-						try {
-							participant = this.business.getParticipantHome().findByYearAndParticipantNumberAndName(yearGroup.getPrimaryKey(), Integer.parseInt(participantNumber), name);
-							user = participant.getUser();
-						}
-						catch (FinderException fe) {
-							System.out.println("User not found, creating...");
-						}
-					}
-					if (user == null) {
-						try {
-							user = this.business.getUserBiz().getUserHome().findByDateOfBirthAndName(birth.getDate(), name);
-						}
-						catch (FinderException fe) {
-							System.out.println("User not found, creating...");
-						}
-					}
-					if (user == null) {
-						try {
-							Gender theGender = this.genderBusiness.getGender(new Integer(gender));
-							Name fullName = new Name(name);
-							user = this.business.getUserBiz().createUser(fullName.getFirstName(), fullName.getMiddleName(), fullName.getLastName(), fullName.getName(), personalID, theGender, birth);
-						}
-						catch (FinderException fe) {
-							System.err.println("Gender not found");
-							return false;
-						}
-						catch (CreateException ce) {
-							ce.printStackTrace();
-							return false;
-						}
-					}
-					
-					if (participant == null) {
-						try {
-							participant = this.business.getParticipantByRunAndYear(user, runGroup, yearGroup);
-						}
-						catch (FinderException fe) {
-							try {
-								participant = this.business.importParticipant(user, runGroup, yearGroup, distanceGroup);
-							}
-							catch (CreateException ce) {
-								ce.printStackTrace();
-								return false;
-							}
-						}
-					}
-					if (participant.getRunDistanceGroupID() != ((Integer)distanceGroup.getPrimaryKey()).intValue()) {
-						Group oldAgeGenderGroup = participant.getRunGroupGroup();
-						Group newAgeGenderGroup = this.business.getAgeGroup(user, runGroup, distanceGroup);
-						
-						Collection userIDs = new ArrayList();
-						userIDs.add(user.getPrimaryKey().toString());
-						userBusiness.moveUsers(IWContext.getInstance(),userIDs, oldAgeGenderGroup, ((Integer)newAgeGenderGroup.getPrimaryKey()).intValue());
-						
-						participant.setRunDistanceGroup(distanceGroup);
-						participant.setRunGroupGroup(newAgeGenderGroup);
-					}
-					participant.setChipNumber(chipNumber);
-					if (country != null) {
-						participant.setUserNationality(country.getName());
-					}
-					if (runGroupName != null && runGroupName.trim().length() > 0) {
-						participant.setRunGroupName(runGroupName);
-					}
-					try {
-						if (charity != null && !charity.trim().equals("")) {
-							Charity charityOgranization = this.charityBusiness.getCharityHome().findByPrimaryKey(Integer.valueOf(charity.trim()));
-							if (charityOgranization!= null) {
-								participant.setCharityId(charityOgranization.getOrganizationalID());
-							}
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-					this.business.updateRunForParticipant(participant, number, runTime, chipTime, split1, split2);
 				}
-				return true;
+			
+				User user = null;
+				Participant participant = null;
+				IWTimestamp birth = new IWTimestamp(dateOfBirth);
+				try {
+					if (!userID.trim().equals("")) {
+						user = this.userBusiness.getUser(Integer.valueOf(userID));
+					}
+				} 
+				catch (Exception e) {
+					System.out.println("User not found by user_id, trying personal id...");
+				}
+				if (user == null) {
+					try {
+						if (personalID.length() > 0) {
+							personalID = TextSoap.findAndReplace(personalID, "-", "");
+							user = this.business.getUserBiz().getUser(personalID);
+						}
+					}
+					catch (FinderException fe) {
+						System.out.println("User not found by personal ID, trying to find participation in run by participant number and full name...");
+					}
+				}
+				if (user == null) {
+					try {
+						participant = this.business.getParticipantHome().findByYearAndParticipantNumberAndName(yearGroup.getPrimaryKey(), Integer.parseInt(participantNumber), name);
+						user = participant.getUser();
+					}
+					catch (FinderException fe) {
+						System.out.println("User not found, trying name and date of birth...");
+					}
+				}
+				if (user == null) {
+					try {
+						user = this.business.getUserBiz().getUserHome().findByDateOfBirthAndName(birth.getDate(), name);
+					}
+					catch (FinderException fe) {
+						System.out.println("User not found, creating...");
+					}
+				}
+				if (user == null) {
+					Gender theGender = this.genderBusiness.getGender(new Integer(gender));
+					Name fullName = new Name(name);
+					user = this.business.getUserBiz().createUser(fullName.getFirstName(), fullName.getMiddleName(), fullName.getLastName(), fullName.getName(), personalID, theGender, birth);
+				}
+				
+				if (participant == null) {
+					try {
+						participant = this.business.getParticipantByRunAndYear(user, runGroup, yearGroup);
+					}
+					catch (FinderException fe) {
+						participant = this.business.importParticipant(user, runGroup, yearGroup, distanceGroup);
+					}
+				}
+				if (participant.getRunDistanceGroupID() != ((Integer)distanceGroup.getPrimaryKey()).intValue()) {
+					Group oldAgeGenderGroup = participant.getRunGroupGroup();
+					Group newAgeGenderGroup = this.business.getAgeGroup(user, runGroup, distanceGroup);
+					
+					Collection userIDs = new ArrayList();
+					userIDs.add(user.getPrimaryKey().toString());
+					userBusiness.moveUsers(IWContext.getInstance(),userIDs, oldAgeGenderGroup, ((Integer)newAgeGenderGroup.getPrimaryKey()).intValue());
+					
+					participant.setRunDistanceGroup(distanceGroup);
+					participant.setRunGroupGroup(newAgeGenderGroup);
+				}
+				participant.setChipNumber(chipNumber);
+				if (country != null) {
+					participant.setUserNationality(country.getName());
+				}
+				if (runGroupName != null && runGroupName.trim().length() > 0) {
+					participant.setRunGroupName(runGroupName);
+				}
+				try {
+					if (charity != null && !charity.trim().equals("")) {
+						Charity charityOgranization = this.charityBusiness.getCharityHome().findByPrimaryKey(Integer.valueOf(charity.trim()));
+						if (charityOgranization!= null) {
+							participant.setCharityId(charityOgranization.getOrganizationalID());
+						}
+					}
+				} catch (FinderException e) {
+						//Charity organization not found
+					}
+				this.business.updateRunForParticipant(participant, number, runTime, chipTime, split1, split2);
 			}
-			catch (RemoteException e1) {
-				e1.printStackTrace();
-				return false;
-			}
+			return true;
 		}
-		return false;
+		catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 	
 
@@ -289,7 +260,7 @@ public class MarathonFileImportHandlerBean extends IBOServiceBean  implements Ma
 	}
 
 	public List getFailedRecords() throws RemoteException {
-		return null;
+		return failedRecords;
 	}
 	
 	protected RunBusiness getBusiness(IWApplicationContext iwac) {
