d59aa6dbc06229ca9fcd692eb820d471997b0f62
Pall Helgason <palli@idega.com>
2011-06-23 15:52:33 +0000

    Moving stuff around for Hlaupastyrkur. Cant call them while inside the transaction because they call us right back to get info.

---
+++ src/java/is/idega/idegaweb/marathon/business/HlaupastyrkurHolder.java
@@ -0,0 +1,26 @@
+package is.idega.idegaweb.marathon.business;
+
+public class HlaupastyrkurHolder {
+	private Runner runner;
+	private String login;
+	private String password;
+	
+	public Runner getRunner() {
+		return runner;
+	}
+	public void setRunner(Runner runner) {
+		this.runner = runner;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}	
+}
